e551769ef5fa26be7bdf066eb49b459b8c016ff6
Mike Francis <mike@benchmark.co.uk>
2015-03-24 12:16:14 +0000

    Change port for prod

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
-var dotenv        = require('dotenv').load();
+var express       = require('express');
+var bodyParser    = require('body-parser');
+var request       = require('request');
+var dotenv        = require('dotenv');
 var SpotifyWebApi = require('spotify-web-api-node');
-var spotifyApi    = new SpotifyWebApi({
+
+dotenv.load();
+
+var spotifyApi = new SpotifyWebApi({
   clientId     : process.env.SPOTIFY_KEY,
   clientSecret : process.env.SPOTIFY_SECRET,
   redirectUri  : process.env.SPOTIFY_REDIRECT_URI
 });
-var scopes = ['playlist-modify-public', 'playlist-modify-private'];
-var code   = '';
 
-var auth = function() {
-  spotifyApi.authorizationCodeGrant(code)
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+app.get('/authorise', function(req, res) {
+  var scopes = ['playlist-modify-public', 'playlist-modify-private'];
+  var state  = new Date().getTime();
+  var authoriseURL = spotifyApi.createAuthorizeURL(scopes, state);
+  res.redirect(authoriseURL);
+});
+
+app.get('/callback', function(req, res) {
+  spotifyApi.authorizationCodeGrant(req.query.code)
     .then(function(data) {
       spotifyApi.setAccessToken(data.body['access_token']);
       spotifyApi.setRefreshToken(data.body['refresh_token']);
+      res.send('You are now authed!');
     }, function(err) {
-      var authoriseURL = spotifyApi.createAuthorizeURL(scopes, console.time());
-      console.log('You probably need to authenticate', authoriseURL);
+      res.send(err);
     });
-};
+});
 
-var findTrack = function(track) {
-  spotifyApi.searchTracks(track)
+app.post('/store', function(req, res) {
+  spotifyApi.refreshAccessToken()
     .then(function(data) {
-      var results = data.body.tracks.items;
-      var trackId = results[0].id;
-      return addToPlaylist(trackId);
+      spotifyApi.searchTracks(req.body.text)
+        .then(function(data) {
+          var results = data.body.tracks.items;
+          var trackId = results[0].id;
+          spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:' + trackId])
+            .then(function(data) {
+              res.send('Track added.');
+            }, function(err) {
+              res.send(err.message);
+            });
+        }, function(err) {
+          res.send(err.message);
+        });
     }, function(err) {
-      console.log('No track found', err);
+      res.send('Could not referesh access token');
     });
-};
+});
 
-var addToPlaylist = function(trackId) {
-  spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:' + trackId])
-    .then(function(data) {
-      console.log('Track added.');
-    }, function(err) {
-      console.error(err);
-    });
-};
-
-findTrack('Bohemian Rhapsody - Queen');
+app.set('port', (process.env.PORT || 3000))
+app.listen(app.get('port'));
