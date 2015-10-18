92ba1c22adc9a0c5d3839a82341f1afe17617a21
TheConnMan <bcconn2112@gmail.com>
2015-10-18 14:24:08 -0400

    Added the username of the song requester for outgoing webhook bot responses.

--- app.js
+++ app.js
@@ -84,7 +84,8 @@ app.post('/store', function(req, res) {
           var track = results[0];
           spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:' + track.id])
             .then(function(data) {
-              return slack(res, 'Track added: *' + track.name + '* by *' + track.artists[0].name + '*');
+              var message = 'Track added' + (process.env.SLACK_OUTGOING ? ' by *' + req.body.user_name + '*' : '') + ': *' + track.name + '* by *' + track.artists[0].name + '*'
+              return slack(res, message);
             }, function(err) {
               return slack(res, err.message);
             });
