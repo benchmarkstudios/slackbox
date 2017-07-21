ef25b7b9d1eae86bbe37d537593c7cb0597293c1
Sam Maxwell <sam@payfriendz.com>
2017-07-21 13:48:00 +0100

    mo logs

--- src/methods/hackathon.js
+++ src/methods/hackathon.js
@@ -9,7 +9,9 @@ const hackathon = res => async query => {
   }
   console.log(tracks)
   const [ track ] = tracks;
+  console.log(track)
   await spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, '1kdf8FDIidEB6NpHTd5Yil', [`spotify:track:${track.id}`]);
+  console.log('added')
   const message = `Track added: *${track.name}* by *${track.artists[0].name}*`;
   return res.send(message)
 }
